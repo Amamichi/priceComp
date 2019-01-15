67ee3b0bf804de90aee7acf2e1b5b7c78cca30fd
MikeTheBigBrother <hddefault520713@gmail.com>
2019-01-16 05:42:21 +0800

    remove unneccesary stuff

--- service/firedata.js
+++
@@ -1,9 +0,0 @@
-//firebase admin
-var admin = require("firebase-admin");
-var serviceAccount = require("./jubeecompare-firebase-adminsdk-f9u14-4d9a4ab82c.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://jubeecompare.firebaseio.com"
-});
-
-module.exports = admin.database();
